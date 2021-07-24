c3138d115363656400f8e9e40d1484d6631ae1a9
Irina <missginger31@gmail.com>
2021-07-24 23:43:05 +0300

    Initial Commit

---
+++ AutoLayouts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayouts
+//
+//  Created by Irunya =} on 24/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
